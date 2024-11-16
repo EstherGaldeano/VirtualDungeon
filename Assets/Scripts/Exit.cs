446e7383404d967c5b928d199c26099b02cb6e62
Dani <deathour@gmail.com>
2024-11-16 20:54:22 +0100

    Creditos y Salir al MenuPrincipal
    Creditos hechos. Falta arreglar el poder manipular el MenuPrincipal después de salir del FPS.

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject FPSController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            SceneManager.LoadScene("MenuPrincipal");
+        }
+    }
+}
--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -1,8 +1,60 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPrincipal : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject ButtonFixedCam;
+
+    [SerializeField]
+    private GameObject ButtonFPSCam;
+
+    [SerializeField]
+    private GameObject ButtonSalir;
+
+    [SerializeField]
+    private GameObject ButtonCredits;
+
+    [SerializeField]
+    private GameObject Credits;
+
+    [SerializeField]
+    private GameObject Atras;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Creditos()
+    {
+        this.ButtonFixedCam.SetActive(false);
+        this.ButtonFPSCam.SetActive(false);
+        this.ButtonSalir.SetActive(false);
+        this.ButtonCredits.SetActive(false);
+        this.Credits.SetActive(true);
+        this.Atras.SetActive(true);
+    }
+
+    public void BackMP()
+    {
+        this.ButtonFixedCam.SetActive(true);
+        this.ButtonFPSCam.SetActive(true);
+        this.ButtonSalir.SetActive(true);
+        this.ButtonCredits.SetActive(true);
+        this.Credits.SetActive(false);
+        this.Atras.SetActive(false);
+    }
 
     public void CambiarEscena(string nombreEscenaDestino)
     {
@@ -22,15 +74,4 @@ public class MenuPrincipal : MonoBehaviour
 
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
