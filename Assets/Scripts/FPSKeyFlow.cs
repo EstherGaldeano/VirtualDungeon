e2889e960c513d86a981b2dd09903b4fe44d3f5e
Andrés <hidalgoandres25@gmail.com>
2024-11-21 17:27:57 +0100

    Gameflow

---
+++ Assets/Scripts/FPSKeyFlow.cs
@@ -0,0 +1,111 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using TMPro;
+
+public class FPSKeyFlow : MonoBehaviour
+{
+    public GameObject door2;
+
+    public TMP_Text text1;
+    public TMP_Text killsText;
+
+    public GameObject key1;
+    public GameObject key2;
+
+    public GameObject key1Img;
+    public GameObject key2Img;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        text1.gameObject.SetActive(false);
+        key1.gameObject.SetActive(false);
+        key2.gameObject.SetActive(false);
+        key1Img.SetActive(false);
+        key2Img.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            GameFlow.updateKills();
+        }
+        if(GameFlow.kills >= 10)
+        {
+            key1.gameObject.SetActive(true);
+            key2.gameObject.SetActive(true);
+        }
+        killsText.text = "KILLS: " + GameFlow.kills.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "GrateDoor" && !GameFlow.key1Obtained)
+        {
+            text1.text = "Necesitas la llave 1 para abrir la puerta";
+            text1.gameObject.SetActive(true);
+        }
+        if (other.gameObject.tag == "Door2" && !GameFlow.key2Obtained)
+        {
+            text1.text = "Necesitas la llave 2 para abrir la puerta";
+            text1.gameObject.SetActive(true);
+        }
+        if (other.gameObject.tag == "Key1")
+        {
+            Destroy(other.gameObject);
+
+            GameFlow.key1Obtained = true;
+            key1Img.SetActive(true);
+        }
+        if (other.gameObject.tag == "Key2")
+        {
+            Destroy(other.gameObject);
+
+            GameFlow.key2Obtained = true;
+            key2Img.SetActive(true);
+        }
+        if (other.gameObject.tag == "GrateDoor" && GameFlow.key1Obtained)
+        {
+            text1.text = "Tienes acceso";
+            text1.gameObject.SetActive(true);
+            Invoke("TextDisappear", 3f);
+
+            Animator anim = other.GetComponent<Animator>();
+            anim.SetTrigger("Accion");
+        }
+        if (other.gameObject.tag == "Door2" && GameFlow.key2Obtained)
+        {
+            text1.text = "Tienes acceso";
+            text1.gameObject.SetActive(true);
+            Invoke("TextDisappear", 3f);
+
+            Animator anim = other.GetComponent<Animator>();
+            anim.SetBool("Accion", true);
+        }
+        if (other.gameObject.tag == "Door2Close")
+        {
+            Animator anim = door2.GetComponent<Animator>();
+
+            anim.SetBool("Accion", false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "GrateDoor" && !GameFlow.key1Obtained)
+        {
+            TextDisappear();
+        }
+        if (other.gameObject.tag == "Door2" && !GameFlow.key2Obtained)
+        {
+            TextDisappear();
+        }
+    }
+
+    private void TextDisappear()
+    {
+        text1.gameObject.SetActive(false);
+    }
+}
